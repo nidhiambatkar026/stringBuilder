ad13f6c352e2b9aaf43d2fb9b5986555f5f0cfcd
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-14 11:23:13 +0530

    Fuctions of stringBuilder class

---
+++ functions.java
@@ -0,0 +1,13 @@
+public class functions {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("Nidhi");
+        System.out.println(sb);
+        
+        //charAt()
+        System.out.println(sb.charAt(0));
+
+        //setCharAt()
+        sb.setCharAt(0, 'S');
+        System.out.println(sb);
+    }
+}
